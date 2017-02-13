3ba8c7acbf50c668233b7e3c66efb0bc9e1acee5
Tony B <hellopeople434@gmail.com>
2017-02-13 13:20:01 -0800

    adding random int

---
+++ randomInCreate.js
@@ -0,0 +1,16 @@
+//randomint
+/save as randomInCreate.js
+var randomInteger = function(val) {
+	var ival = Math.random()*356;
+	//console.log(ival);
+	Ival = Math.floor(ival);
+	return ical;
+}
+//main
+var rInt;
+var i;
+for (i = 0; i < 65; i++){
+	rInt = randomInteger();
+	//console.log(rInt+",");
+	process.stdout.write(rInte+",");
+}
